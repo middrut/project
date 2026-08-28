8b716d55cef3b074bf71757a92a5568d90e5803e
middrut <middrut3022@gmail.com>
2026-08-28 16:24:14 +0530

    -m "Add CSR website frontend and media"

--- CSRMarketingWebsite/src/main/resources/static/js/script.js
+++ CSRMarketingWebsite/src/main/resources/static/js/script.js
@@ -1,1 +1,23 @@
-console.log("CSR Marketing Website Loaded Successfully");
+document.addEventListener("DOMContentLoaded", function () {
+
+    const music = document.getElementById("backgroundMusic");
+    const button = document.getElementById("musicButton");
+
+    button.addEventListener("click", function () {
+
+        if (music.paused) {
+
+            music.play();
+
+            button.textContent = "Pause Music";
+
+        } else {
+
+            music.pause();
+
+            button.textContent = "Play Music";
+        }
+
+    });
+
+});
